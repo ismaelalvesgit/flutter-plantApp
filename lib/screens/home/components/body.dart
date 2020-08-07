47a814e8281b943ce33e38ff835b538d17c63f62
ismaelalvesgit <cearaismael1997@gmail.com>
2020-08-07 09:29:05 -0300

    projeto finalizado

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
+import 'featurred_plants.dart';
+import 'header_with_seachbox.dart';
+import 'recomend_plants.dart';
+import 'title_with_more_bbtn.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // It will provie us total height  and width of our screen
+    Size size = MediaQuery.of(context).size;
+    // it enable scrolling on small device
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(title: "Recomendado", press: () {}),
+          RecomendsPlants(),
+          TitleWithMoreBtn(title: "Plantas em destaque", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
+        ],
+      ),
+    );
+  }
+}
